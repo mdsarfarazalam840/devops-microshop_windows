3299eeb5111ef20e51a4917c651abcc39c9cc126
mdsarfarazalam840 <md.sarfarazalm840@gmail.com>
2025-09-10 21:10:52 +0530

    Updated some files

--- app/app.js
+++ app/app.js
@@ -1,21 +1,32 @@
 const express = require('express');
+const client = require('prom-client');
 
-const PORT = process.env.PORT || 3000;
 const app = express();
+const collectDefaultMetrics = client.collectDefaultMetrics;
+collectDefaultMetrics(); // default metrics
 
-app.get('/healthz', (req, res) => {
-  res.status(200).json({ status: 'ok' });
+const httpRequestDurationMs = new client.Histogram({
+  name: 'http_request_duration_ms',
+  help: 'Duration of HTTP requests in ms',
+  labelNames: ['method','route','code'],
+  buckets: [50, 100, 200, 300, 500, 1000]
 });
 
-app.get('/', (req, res) => {
-  res.status(200).json({ message: 'Welcome to devops-microshop!' });
+app.use((req, res, next) => {
+  const end = httpRequestDurationMs.startTimer();
+  res.on('finish', () => {
+    end({ method: req.method, route: req.path, code: res.statusCode });
+  });
+  next();
 });
 
-app.listen(PORT, () => {
-  // eslint-disable-next-line no-console
-  console.log(`Server listening on port ${PORT}`);
+app.get('/', (req, res) => res.send('Hello from DevOps microservice!'));
+app.get('/health', (req, res) => res.json({status:'ok'}));
+
+app.get('/metrics', async (req, res) => {
+  res.set('Content-Type', client.register.contentType);
+  res.end(await client.register.metrics());
 });
 
-module.exports = app;
-
-
+const port = process.env.PORT || 3000;
+app.listen(port, () => console.log(`App listening on ${port}`));
